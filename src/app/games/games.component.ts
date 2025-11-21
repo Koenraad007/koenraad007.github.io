a77848970a306ff5fdcd26caae1a745ef7c57fff
KoenVA <s144435@ap.be>
2025-11-21 23:52:44 +0100

    feat: Add Games component

---
+++ src/app/games/games.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-games',
+  standalone: true,
+  imports: [],
+  templateUrl: './games.component.html',
+  styleUrls: ['./games.component.css'],
+})
+export class GamesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
