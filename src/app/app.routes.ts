aa2e8e1ce76475b1f9a9fcfb8aef0919d13f5845
KoenVA <s144435@ap.be>
2025-11-21 23:54:23 +0100

    feat: Add route for Games component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { GamesComponent } from './games/games.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'games', component: GamesComponent },
 ];
